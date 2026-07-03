0179e266fc501a9113d8d71dac530240a0c34e77
prashanthkalasani <prashanthkalasani143@gmail.com>
2026-07-03 17:05:41 +0530

    hellow-world-2

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# No space before or after equal
+PERSON1=Naveen
+PERSON2=Pavan
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning Shell Scripting $PERSON1"
