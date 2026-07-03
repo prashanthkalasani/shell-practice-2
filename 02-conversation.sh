1973acd9233393cb360c3b2b47ab316056de5afb
prashanthkalasani <prashanthkalasani143@gmail.com>
2026-07-03 16:56:30 +0530

    hellow-world-2

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Naresh:: Hi Naresh, How are you?"
+echo "Prashanth:: Hello Naresh, I am fine. How are you?"
+echo "Naresh:: What are you learning recently?"
+echo "Prashanth:: I am learning Shell Scripting Naresh"
